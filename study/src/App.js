7f58914bb4a6ebb1b227da76a7c2f4f2420d21cb
leejh4197 <leejh4197@naver.com>
2022-03-31 16:50:14 +0900

    [ClassFunc]

--- study/src/App.js
+++ study/src/App.js
@@ -2,17 +2,20 @@ import React from 'react';
 import './App.css';
 import LifecycleEx from './studyChart/04_라이프사이클/LifecycleEx';
 import Callbackhell from './studyChart/06_Callback/async/callbackhell';
+import FunctionEx from './studyChart/05_클래스형vs함수형/FunctionEx';
+import ClassEx from './studyChart/05_클래스형vs함수형/ClassEx';
 import { Route } from 'react-router-dom';
 
 function App() {
-  const [is_cat, setIsCat] = React.useState(true);
+  // const [is_cat, setIsCat] = React.useState(true);
   return (
-    <div className="App">
-      <Route exact>
-      {is_cat ? <LifecycleEx/> : null}
-      <button onClick={() => {setIsCat(!is_cat)}}>바꾸기</button>
-      </Route>
+    <div className="container">
+      {/* {is_cat ? <LifecycleEx/> : null}
+      <button onClick={() => {setIsCat(!is_cat)}}>바꾸기</button> */}
       <Route component={Callbackhell} path="/callback" exact/>
+      <h1>Hello World</h1>
+      <FunctionEx initNumber={2}/>
+      <ClassEx initNumber={2}/>
     </div>
   );
 }
--- study/src/studyChart/05_클래스형vs함수형/Desc.md
+++ study/src/studyChart/05_클래스형vs함수형/Desc.md
@@ -1,1 +1,38 @@
 ### 클래스형 vs 함수형
+
+__클래스형__
+
+- React 컴포넌트에서 선언하는 두가지 방식 중 하나이다
+- 현재 자주 사용하지 않지만 기존 프로젝트의 유지보수를 위해 클래스형 컴포넌트의 개념을 알아두어야 한다.
+
+**클래스형 컴포넌트 예시**
+```
+class App extends Component{
+    render(){
+        const name = 'react';
+        return <div className="react">{name}</div>
+    }
+}
+
+export default APP;
+```
+
+1. Class 키워드가 필요하다.(class)
+2. Component로 상속을 받아야한다.(App extends Component)
+3. reder() 메소드가 반드시 있어야한다.(render())
+
+**함수형 컴포넌트 예시**
+```
+const a = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default a;
+```
+
+1. 자기 자신자체가 render함수가 된다.
+2. State, lifeCycle을 Hook을 통해 해소하였다.
